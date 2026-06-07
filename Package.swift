21f1d0eeb9d33f224df5c07a8ebf4b7742d0add2
Alexey Demin <git@dnv1ex.com>
2026-06-07 14:43:13 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,10 +9,7 @@ let package = Package(
     .executable(name: "media-generation-kit-cli", targets: ["MediaGenerationKitCLI"]),
   ],
   dependencies: [
-    .package(
-      url: "https://github.com/drawthingsai/draw-things-community.git",
-      revision: "d473a2f148b3e7dc9b90d0b7cfccc5cda999eb66"
-    ),
+    .package(url: "https://github.com/DnV1eX/draw-things-community.git", branch: "forked-dependencies"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.1"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5"),
   ],
